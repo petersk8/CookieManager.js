2b376a9b904a6c05c1a7f2c451c5a1eb48fbe45d
PeBo <petersk8@yahoo.com>
2017-11-07 11:38:40 +0000

    first commit
    first commit of the repo

---
+++ CookieManager.js
@@ -0,0 +1,21 @@
+﻿function setCookie(cname, cvalue, expTimeHours) {
+    var d = new Date();
+    d.setTime(d.getTime() + (expTimeHours * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
